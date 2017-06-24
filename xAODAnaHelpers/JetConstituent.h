dc1204470e0df78718341f25a7860895d86fb9c5
Karol Krizka <kkrizka@gmail.com>
2017-06-23 23:00:02 -0700

    Compiles with JetHelpTree.

---
+++ xAODAnaHelpers/JetConstituent.h
@@ -0,0 +1,18 @@
+#ifndef xAODAnaHelpers_JetConstituent_H
+#define xAODAnaHelpers_JetConstituent_H
+
+#include "xAODAnaHelpers/Particle.h"
+#include "xAODAnaHelpers/TrackParticle.h"
+#include "xAODAnaHelpers/MuonContainer.h"
+
+
+namespace xAH {
+
+  class JetConstituent : public Particle
+  {
+  public:      
+    JetConstituent();
+  };
+
+} //xAH
+#endif // xAODAnaHelpers_JetConstituent_H
